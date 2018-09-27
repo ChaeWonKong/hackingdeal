7bc5164c9a2cd02e9067c7f73eb0b0d4117b6bc7
공채원 <chaewonkong@gmail.com>
2018-09-27 12:07:30 +0900

    Add static and index.html to default route

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Index.js
+const express = require("express");
+const path = require("path");
+
+const app = express();
+
+app.use(express.static(__dirname + "/client"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "client", "/index.html"));
+});
+
+app.listen(4000, () => {
+  console.log("Server is running on:4000");
+});
