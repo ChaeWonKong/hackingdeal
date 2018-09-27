af04951f8eb9e3dd7c47830690350f0beec39eca
공채원 <chaewonkong@gmail.com>
2018-09-27 12:45:30 +0900

    Add routing to detail page from json data

--- index.js
+++ index.js
@@ -1,12 +1,24 @@
 const express = require("express");
 const path = require("path");
+const fs = require("fs");
 
 const app = express();
 
 app.use(express.static(__dirname + "/client"));
 
+// Base Route
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "client", "/index.html"));
+});
+
+// Detail Route
+app.get("/:pageId", (req, res) => {
+  fs.readFile(path.join(__dirname + "/data/items.json"), (err, data) => {
+    if (err) {
+      throw err;
+    }
+    res.send(JSON.parse(data).Deals[req.params.pageId - 1]);
+  });
 });
 
 app.listen(4000, () => {
