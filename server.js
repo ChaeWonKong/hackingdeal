e1e2b1e5df9ef8d1bdfdfaedeea733f6d63155d3
공채원 <chaewonkong@gmail.com>
2018-09-29 09:31:54 +0900

    Fix bug in server.js

--- server.js
+++ server.js
@@ -8,27 +8,25 @@ const app = express();
 app.use(express.static(path.join(__dirname, "/client")));
 
 // Detail Route
-// app.get("/:pageId", (req, res) => {
-//   fs.readFile(path.join(__dirname + "/data/items.json"), (err, data) => {
-//     if (err) {
-//       throw err;
-//     }
-//     // res.send(JSON.parse(data).Deals[req.params.pageId - 1]);
-//     else {
-//       const itemId = String(Number(req.params.pageId) - 1);
-//       const item = JSON.parse(data).Deals[itemId];
-//       const html = detail.HTML(
-//         item.title,
-//         item.price,
-//         item.img,
-//         item.description,
-//         item.url
-//       );
-//       res.sendFile(path.join(__dirname, "/client"));
-//       res.send(html);
-//     }
-//   });
-// });
+app.get("/:pageId", (req, res) => {
+  fs.readFile(path.join(__dirname + "/data/items.json"), (err, data) => {
+    if (err) {
+      throw err;
+    } else {
+      const itemId = String(Number(req.params.pageId) - 1);
+      const item = JSON.parse(data).Deals[itemId];
+      const html = detail.HTML(
+        item.title,
+        item.price,
+        item.img,
+        item.description,
+        item.url
+      );
+      res.sendFile(path.join(__dirname, "/client"));
+      res.send(html);
+    }
+  });
+});
 
 // Base Route
 app.get("/", (req, res) => {
@@ -37,7 +35,6 @@ app.get("/", (req, res) => {
       throw err;
     } else {
       const items = JSON.parse(data).Deals;
-      console.log(items);
       const html = index.HTML(items);
       res.sendFile(path.join(__dirname, "/client"));
       res.send(html);
