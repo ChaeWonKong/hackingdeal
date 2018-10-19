f3a052decea3550ac86f3e27d479659a180973c9
공채원 <chaewonkong@gmail.com>
2018-10-19 17:39:44 +0900

    Refactor server.js

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const _ = require("lodash");
 const AWS = require("aws-sdk");
 const multer = require("multer");
 const multerS3 = require("multer-s3");
+
 // Image Upload
 AWS.config.loadFromPath(__dirname + "/config/awsconfig.json");
 const s3 = new AWS.S3();
@@ -33,6 +34,62 @@ app.get("/new", (req, res) => {
   const html = template.create();
   res.sendFile(path.join(__dirname, "/public"));
   res.send(html);
+});
+
+// Create Process
+app.post("/create", upload.single("uploaded"), (req, res) => {
+  const body = req.body;
+  fs.readFile(path.join(__dirname + "/data/db.json"), (err, data) => {
+    if (err) throw err;
+    let parsedData = JSON.parse(data).deals;
+    const latestItem = parsedData.length - 1;
+    const image = req.file.key
+      ? "https://s3.ap-northeast-2.amazonaws.com/hackingdeal/" + req.file.key
+      : body.img;
+    const newData = {
+      id: String(Number(parsedData[latestItem].id) + 1),
+      title: body.title,
+      price: body.price,
+      img: image,
+      description: body.description,
+      url: body.url,
+      comments: []
+    };
+    parsedData.push(newData);
+    const DATA = JSON.stringify({ deals: parsedData }, null, 3);
+
+    fs.writeFile(path.join(__dirname + "/data/db.json"), DATA, err => {
+      if (err) throw err;
+      res.redirect(302, "/");
+    });
+  });
+});
+
+// Comment Process
+app.post("/comment/:pageId", (req, res) => {
+  fs.readFile(path.join(__dirname + "/data/db.json"), (err, data) => {
+    if (err) throw err;
+
+    let parsedData = JSON.parse(data);
+    let targetIndex = _.indexOf(
+      parsedData.deals,
+      _.find(parsedData.deals, { id: req.params.pageId })
+    );
+    let comments = parsedData.deals[targetIndex].comments;
+    const newId = comments.length;
+    const body = req.body;
+    comments.push({
+      id: newId,
+      nickName: body.nickName,
+      content: body.content
+    });
+    const DATA = JSON.stringify(parsedData, null, 3);
+
+    fs.writeFile(path.join(__dirname + "/data/db.json"), DATA, err => {
+      if (err) throw err;
+      res.redirect(302, `/${req.params.pageId}`);
+    });
+  });
 });
 
 // data page
@@ -101,62 +158,6 @@ app.get("/:pageId", (req, res) => {
         res.send("oops");
       }
     }
-  });
-});
-
-// Comment Process
-app.post("/comment/:pageId", (req, res) => {
-  fs.readFile(path.join(__dirname + "/data/db.json"), (err, data) => {
-    if (err) throw err;
-
-    let parsedData = JSON.parse(data);
-    let targetIndex = _.indexOf(
-      parsedData.deals,
-      _.find(parsedData.deals, { id: req.params.pageId })
-    );
-    let comments = parsedData.deals[targetIndex].comments;
-    const newId = comments.length;
-    const body = req.body;
-    comments.push({
-      id: newId,
-      nickName: body.nickName,
-      content: body.content
-    });
-    const DATA = JSON.stringify(parsedData, null, 3);
-
-    fs.writeFile(path.join(__dirname + "/data/db.json"), DATA, err => {
-      if (err) throw err;
-      res.redirect(302, `/${req.params.pageId}`);
-    });
-  });
-});
-
-// Create Process
-app.post("/create", upload.single("uploaded"), (req, res) => {
-  const body = req.body;
-  fs.readFile(path.join(__dirname + "/data/db.json"), (err, data) => {
-    if (err) throw err;
-    let parsedData = JSON.parse(data).deals;
-    const latestItem = parsedData.length - 1;
-    const image = req.file.key
-      ? "https://s3.ap-northeast-2.amazonaws.com/hackingdeal/" + req.file.key
-      : body.img;
-    const newData = {
-      id: String(Number(parsedData[latestItem].id) + 1),
-      title: body.title,
-      price: body.price,
-      img: image,
-      description: body.description,
-      url: body.url,
-      comments: []
-    };
-    parsedData.push(newData);
-    const DATA = JSON.stringify({ deals: parsedData }, null, 3);
-
-    fs.writeFile(path.join(__dirname + "/data/db.json"), DATA, err => {
-      if (err) throw err;
-      res.redirect(302, "/");
-    });
   });
 });
 
