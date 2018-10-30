e4e2284b80e34b234fa99784d2627d39b2b9be1f
공채원 <chaewonkong@gmail.com>
2018-10-30 19:34:54 +0900

    Delete unused imports from server.js

--- server.js
+++ server.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const path = require("path");
-const fs = require("fs");
-const template = require("./public/template");
 const bodyParser = require("body-parser");
-const _ = require("lodash");
-const uuidv1 = require("uuid/v1");
 
 // Import Routers
 const indexRouter = require("./routes/index");
@@ -30,32 +26,5 @@ app.get("/data", (req, res) => {
 });
 
 // Update Route
-
-// // Detail Route
-// app.get("/:pageId", (req, res) => {
-//   fs.readFile(path.join(__dirname + "/data/db.json"), (err, data) => {
-//     if (err) throw err;
-//     else {
-//       const { parsedData, targetIndex } = getDataAndIndex(req, data);
-//       const item = parsedData[targetIndex];
-//       if (item) {
-//         const html = template.detail(
-//           item.id,
-//           item.title,
-//           item.price,
-//           item.img,
-//           item.description,
-//           item.url,
-//           item.comments,
-//           item.relatedItems
-//         );
-//         res.sendFile(path.join(__dirname, "/public"));
-//         res.send(html);
-//       } else {
-//         res.send("oops");
-//       }
-//     }
-//   });
-// });
 
 module.exports = app;
